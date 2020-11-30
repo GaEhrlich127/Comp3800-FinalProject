becfe9038b2454493fcf31587b31d7f32f6fdd40
Chan-Rodney <31736541+Chan-Rodney@users.noreply.github.com>
2020-11-30 01:46:18 -0500

    Update voting.sol

--- voting.sol
+++ voting.sol
@@ -2,16 +2,17 @@ pragma solidity ^ 0.5.0;
 pragma experimental ABIEncoderV2;
 contract Vote
 {
-    struct voter
+    struct Voter
     {
         address voterAddress;
-        //mapping(uint32 => bytes32) votes;
-        string[] votes;
+        string[] votes;//change from mapping to array
         bool hasVoted;
         bytes32 info;
     }
+    mapping(string => uint)  tempCandidate;
+    address[] voterAddressArray;
     string[] candidateList;
-    mapping(address => voter) voterList;
+    mapping(address => Voter) voterList;
     address Manager;
     
     constructor(address _manager, string[] memory _candidateList) public
@@ -24,57 +25,107 @@ contract Vote
     {
        
         uint i = 0;
-        for(i = 0; i < votingPreference.length;i++)
+        for(i = 0; i < votingPreference.length; i++)
         {
             voterList[msg.sender].votes[i] = votingPreference[i];
         }
+        voterAddressArray.push(msg.sender);
         
     }
-
-     function compareStrings(string memory a, string memory b) public view returns (bool) //referenced from stack overflow
-     {
-
+    
+    function compareStrings(string memory a, string memory b) public view returns (bool) //referenced off stack overflow
+    {
+   
     return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
-
+    
     }   
 
     function finalizeVote() public
     {
-         for(uint i = 0; i < voterAddressArray.length; i++)//loop through every voter
+       
+        for(uint i = 0; i < candidateList.length;i++)
         {
-            for(uint j =0; j < 1;j++)
-            {
-                if(compareStrings(voterList[voterAddressArray[i]].votes[0],candidateList[0]) == true)
-                {
-                    
+            tempCandidate[candidateList[i]] = 0;//initialize mapping
+        }
+        for(int currentMaxPreference=0; currentMaxPreference<candidateList.length; currentMaxPreference++){
+
+	        //Iterate through each voter
+            for(int voterIndex=0; voterIndex<voterAddressArray.length;i++){
+		        Voter memory currentVoter = voterList[voterIndex];
+		        boolean voteCast=false;
+
+		    //Iterate through the currentVoters vote Preference
+		        for(int voterPreference=0; voterPreference<currentVoter.votes.length; voterPreference++){
+		            if(voteCast==true){break;}
+		            
+		            if(voterPreference > currentMaxPreference){break;}
+		            
+			        if(tempCandidate[candidateList[i]]<0){
+				       
+				       continue;
+			        }
+			        else{
+				        tempCandidate[candidateList[i]]++;
+				        voteCast=true;
+			        }
+			        
+		        }
+            }
+
+            //Eliminate the lowest voted candidate
+	        string[] eliminatedCandidates;
+	        //Start by assuming that the minimum votes achieved were the maximum possible
+	        int lowVotes=voterList.length;
+
+	        //Traverse through each candidates votes
+            for(int i=0;i<candidateList.length;i++){
+		            string currentCandidate=candidateList[i];
+		
+		            //If the current candidate matched the low vote, push them onto the eliminated group
+		            if(tempCandidate[currentCandidate]==lowVotes){
+		                
+			            eliminatedCandidates.push(currentCandidate);
+		            }
+		            //If someone got lower, mark the new low vote, clear the eliminated group, and add them to the empty pile
+		            else if(tempCandidate[currentCandidate]  <lowVotes && tempCandidate[currentCandidate]>=0){
+		                
+			            lowVotes=tempCandidate[currentCandidate];
+			            
+			            for(int j=eliminatedCandidates.length-1;j>=0;j++){
+			                
+				            delete eliminatedCandidate[j]; 
+			            	eliminatedCandidate.length--;
+			            }
+			            eliminatedCandidates.push(currentCandidate);
+		            }
                 }
-                else if(compareStrings(voterList[voterAddressArray[i]].votes[0],candidateList[1]) == true)
-                {
-                    
+
+	    //After, mark all the eliminated Candidates as such
+	       for(int i=0;i<eliminatedCandidates.length;i++){
+	           
+		        tempCandidate[eliminatedCandidates[i]]=-1;
+	        }
+	        for(int i=0;i<candidateList.length;i++){
+                if(tempCandidate[candidateList[i]]>=0){
+                    tempCandidate[candidateList[i]]=0;
                 }
-                else if(compareStrings(voterList[voterAddressArray[i]].votes[0],candidateList[2]) == true)
-                {
-                    
-                }
-                else if(compareStrings(voterList[voterAddressArray[i]].votes[0],candidateList[3]) == true)
-                {
-                    
-                }
-                else if(compareStrings(voterList[voterAddressArray[i]].votes[0],candidateList[4]) == true)
-                {
-                    
-                }
-                
             }
         }
+        for(int i=0;i<candidateList.length;i++){
+	        if(tempCandidate[candidateList[i]]>0){
+		    //Do something here... This is the winner!
+	        }
+        }
     }
+    
     function addCandidate(string memory candidateName) public
     {
         candidateList.push(candidateName);
        
         
     }
-     function getCandidateList() public view returns(string[] memory) 
+    
+    function getCandidateList() public view returns(string[] memory) 
     {
         return candidateList;
     }
